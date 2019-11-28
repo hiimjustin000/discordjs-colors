a018aa7952f938dea7b2965733253e3ffebc5ce5
hiimjustin000 <52604018+hiimjustin000@users.noreply.github.com>
2019-11-28 12:21:06 -0500

    Inital commit

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# The Changelog
+
+## 1.1.0
+Interface improved.\
+Markdown file to show colors.\
+A .d.ts file.\
+Changed name, description, and license.\
+License is now a markdown file.
+
+## 1.0.2
+Fixed a bug where it opened with the program specified with a JavaScript file.
+
+## 1.0.1
+Fixed a bug where it wouldn't run.
+
+## 1.0.0
+Inital release
---
+++ index.d.ts
@@ -0,0 +1,31 @@
+declare module "discordjs-colors" {
+    export let defaultColor: string;
+    export let white: string;
+    export let aqua: string;
+    export let green: string;
+    export let blue: string;
+    export let purple: string;
+    export let luminousvividpink: string;
+    export let gold: string;
+    export let orange: string;
+    export let red: string;
+    export let gray: string;
+    export let navy: string;
+    export let darkaqua: string;
+    export let darkgreen: string;
+    export let darkblue: string;
+    export let darkpurple: string;
+    export let darkvividpink: string;
+    export let darkgold: string;
+    export let darkorange: string;
+    export let darkred: string;
+    export let darkgray: string;
+    export let darkergray: string;
+    export let lightgray: string;
+    export let darknavy: string;
+    export let blurple: string;
+    export let grayple: string;
+    export let darkbutnotblack: string;
+    export let notquiteblack: string;
+    export let randomColor: string;
+}
